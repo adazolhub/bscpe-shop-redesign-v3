3b14592983454c4b5f0823021b865eb59496b30f
Danyel <dpadayao40@gmail.com>
2022-06-20 18:07:34 +0800

    initial commit - Header navigation and mobile navigation designing

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import Heading from "./components/Heading"
+import Navigation from "./components/Navigation"
+
+const Home = () => {
+
+  return(
+    <>
+      <Heading/>
+      <main className="min-h-[calc(100vh-48px)] grid place-content-center">
+        <h1 className="text-3xl font-thin">Hello world</h1>
+
+      </main>
+      <Navigation/>
+    </>
+  )
+}
+
+
+export default Home
