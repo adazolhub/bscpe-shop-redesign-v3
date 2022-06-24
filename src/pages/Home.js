9ffbeac31e7f22b1212a53b767f324bd4827e40e
Danyel <dpadayao40@gmail.com>
2022-06-24 20:06:07 +0800

    sticky navbar & toggle side-menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,28 @@
+import { useState } from "react"
 import Heading from "../components/Heading"
 import Sidebar from "../components/Sidebar"
 
 const Home = () => {
+  const [isOpen, setOpen] = useState(false)
 
-  return(
+
+  return (
     <>
-      <Heading/>
-      <main className="min-h-[calc(100vh-48px)] grid place-content-center">
-        <h1 className="text-3xl font-thin">Hello world</h1>
+      <Heading setOpen={setOpen} />
+      <main className="w-full">
+        <section className="min-h-[calc(100vh-48px)] grid place-content-center">
+          <h1 className="text-3xl font-thin">Hello world</h1>
+        </section>
+        <section className="min-h-[calc(100vh-48px)] grid place-content-center">
+          <h1 className="text-3xl font-thin">Hello world</h1>
+        </section>
+        <section className="min-h-[calc(100vh-48px)] grid place-content-center">
+          <h1 className="text-3xl font-thin">Hello world</h1>
+        </section>
 
       </main>
       {/* <Navigation/> */}
-      <Sidebar/>
+      <Sidebar isOpen={isOpen} setOpen={setOpen} />
     </>
   )
 }
